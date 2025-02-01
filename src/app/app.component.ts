ff9bd839e32e6bd22079ed152846d54c5db0eabd
Filip Strandberg <filip.strandberg@hyph.com>
2025-02-01 14:31:41 +0100

    setup angular material theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { ChangeDetectionStrategy, Component, importProvidersFrom } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { FitTextDirective } from './directives/fit-text.directive'
-import { InputComponent } from "./components/input/input.component";
-import { WINDOW } from './injection-tokens';
+import { MatInputModule } from '@angular/material/input';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FitTextDirective, InputComponent],
+  imports: [
+    RouterOutlet,
+    FitTextDirective,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline', floatLabel: 'always', color: 'primary', subscriptSizing: 'dynamic' },
+    },
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
