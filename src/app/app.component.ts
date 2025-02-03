09a8b68fa3132688c60d7c7af735f14bffc840a2
Filip Strandberg <filip.strandberg@hyph.com>
2025-02-03 10:42:03 +0100

    add countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, Signal } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { FitTextDirective } from './directives/fit-text.directive'
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { DateTime, DurationObjectUnits, DurationUnits } from 'luxon'
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
-import { filter, map } from 'rxjs';
+import { DateTime, DurationObjectUnits } from 'luxon'
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map, startWith, switchMap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -28,28 +28,26 @@ export class AppComponent {
   timeUntilEvent$: Signal<DurationObjectUnits | undefined>;
 
   eventForm = new FormGroup({
-    name: new FormControl<string | undefined>(undefined),
+    title: new FormControl<string | undefined>(undefined),
     date: new FormControl<DateTime | undefined>(undefined),
   });
 
-  readonly dateDiffUnits: DurationUnits = ['days', 'hours', 'minutes', 'seconds'];
+  readonly defaultEventTitle = 'Midsummer 2025'
+  readonly defaultDateStr = '2025-06-20'
 
   constructor() {
     this.timeUntilEvent$ = toSignal(this.dateControl.valueChanges.pipe(
-      takeUntilDestroyed(),
+      startWith(DateTime.fromISO(this.defaultDateStr)),
       filter((date): date is DateTime => !!date),
-      map((date: DateTime) => date.diffNow(this.dateDiffUnits).toObject()),
-      map((timeUntilEvent: DurationObjectUnits) => this.mapDurationUnitSeconds(timeUntilEvent))
-    ),
-    { initialValue: this.mapDurationUnitSeconds(DateTime.now().plus({ years: 0.5 }).diffNow(this.dateDiffUnits).toObject()) });
+      switchMap((date: DateTime) => timer(0, 1000).pipe(
+        map(() => date.diffNow(['days', 'hours', 'minutes', 'seconds']).toObject()),
+        map((timeUntilEvent: DurationObjectUnits) => ({ ...timeUntilEvent, seconds: ~~(timeUntilEvent.seconds || 0) }))
+      )),
+    ));
   }
 
-  mapDurationUnitSeconds(timeUntilEvent: DurationObjectUnits) {
-    return ({ ...timeUntilEvent, seconds: ~~(timeUntilEvent.seconds || 0) })
-  }
-
-  get nameControl(): FormControl {
-    return this.eventForm.controls['name'];
+  get titleControl(): FormControl {
+    return this.eventForm.controls['title'];
   }
 
   get dateControl(): FormControl {
