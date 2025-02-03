44578e9c2444dd4fe11cd9db70f32f88ac5b995e
Filip Strandberg <filip.strandberg@hyph.com>
2025-02-03 11:00:41 +0100

    add localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Signal } from '@angular/core'
+import { AfterViewInit, ChangeDetectionStrategy, Component, Signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { FitTextDirective } from './directives/fit-text.directive'
 import { MatInputModule } from '@angular/material/input';
@@ -6,8 +6,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { DateTime, DurationObjectUnits } from 'luxon'
-import { toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { filter, map, startWith, switchMap, timer } from 'rxjs';
+import { LocalStorageService } from './services/local-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -35,7 +36,10 @@ export class AppComponent {
   readonly defaultEventTitle = 'Midsummer 2025'
   readonly defaultDateStr = '2025-06-20'
 
-  constructor() {
+  readonly localStorageKeyTitle = 'eventTitle';
+  readonly localStorageKeyDate = 'eventDate';
+
+  constructor(private localStorageService: LocalStorageService) {
     this.timeUntilEvent$ = toSignal(this.dateControl.valueChanges.pipe(
       startWith(DateTime.fromISO(this.defaultDateStr)),
       filter((date): date is DateTime => !!date),
@@ -44,6 +48,26 @@ export class AppComponent {
         map((timeUntilEvent: DurationObjectUnits) => ({ ...timeUntilEvent, seconds: ~~(timeUntilEvent.seconds || 0) }))
       )),
     ));
+
+    const storedTitle = this.localStorageService.getSavedState(this.localStorageKeyTitle);
+    const storedDate = this.localStorageService.getSavedState(this.localStorageKeyDate);
+
+    if(storedTitle) {
+      this.titleControl.setValue(storedTitle)
+    }
+
+    if(storedDate) {
+      this.dateControl.setValue(DateTime.fromISO(storedDate))
+    }
+
+    this.dateControl.valueChanges.pipe(
+      takeUntilDestroyed(),
+      filter((date): date is DateTime => !!date),
+    ).subscribe((date: DateTime) => this.localStorageService.setSavedState(this.localStorageKeyDate, date.toISO()!))
+
+    this.titleControl.valueChanges.pipe(
+      takeUntilDestroyed()
+    ).subscribe((title: string) => this.localStorageService.setSavedState(this.localStorageKeyTitle, title))
   }
 
   get titleControl(): FormControl {
---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,17 @@
+import { inject, Inject, Injectable } from '@angular/core';
+import { WINDOW } from '../injection-tokens';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+  window = inject(WINDOW);
+
+  setSavedState(localStorageKey: string, state: string) {
+    this.window.localStorage.setItem(localStorageKey, state);
+  }
+
+  getSavedState(localStorageKey: string): string | null {
+    return this.window.localStorage.getItem(localStorageKey);
+  }
+}
